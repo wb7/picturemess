0fe6f164e11291961034b47646e35aab05c9ae9b
wb7 <lg@wb7.eu>
2013-11-20 18:57:25 +0000

    Added test images for second album, auto generated desc files, added generator for desc files

--- php.class.php
+++ php.class.php
@@ -2,13 +2,71 @@
 
 class picturemess {
 
-	$dir; //root dir
-	$content; //Content of XML file (albums)
+	private $dir; //root dir
+	private $content; //Content of XML file (albums)
+	private $xml; //simple xml
 
 	public function __construct($dir = ".")
 	{
 
 		$this->$dir = $dir;
+
+	}
+
+	public function init()
+	{
+
+		if (@fopen($this->dir . "albums.xml", "r"))
+		{
+			$this->content = file_get_contents($this->dir . "albums.xml");
+			$this->xml = simplexml_load_string($this->content);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+
+	public function createFiles()
+	{
+		
+		foreach($this->xml as $row)
+		{
+			$content = "<album>\r\n";
+			$content .= $this->createDescXML($this->dir . "images/" . $row->folder);
+			$content .= "</album>";	
+
+			if(@$handle = fopen($this->dir . "desc/" . $row->folder . ".xml", "w"))
+			{
+				fwrite($handle, $content);
+				fclose($handle);
+				echo "(Over-)wrote description files for album \"" . $row->title . "\".\r\n";			
+			}
+			
+		}
+	
+	}
+
+	private function createDescXML($path)
+	{
+	
+		$files = scandir($path);
+		unset($files[0]);
+		unset($files[1]);
+
+		$file = "";
+
+		foreach ($files as $row)
+		{
+			$file .= "  <file>\r\n";
+			$file .= "    <filename>" . $row . "</filename>\r\n";
+			$file .= "    <description></description>\r\n";
+			$file .= "  </file>\r\n";
+		}
+		
+		return $file;
 
 	}
 
--- php.php
+++ php.php
@@ -3,5 +3,11 @@
 include('php.class.php');
 
 $pm = new picturemess(".");
+$init = $pm->init();
 
+// check init
+if ($init){echo "albums.xml loaded.\n";}else{echo "Something went wrong! :("; die;}
+
+// check if user wants to create desc files for each album
+if ($argv[1] == "--create-files"){$pm->createFiles();}
 ?>
