6c1dc9f3586d121e1250fa3294a64d600513a946
wb7 <lg@wb7.eu>
2013-11-20 17:57:48 +0000

    Added php constructor, added init of php class, added sample pics

--- php.class.php
+++ php.class.php
@@ -2,7 +2,15 @@
 
 class picturemess {
 
+	$dir; //root dir
+	$content; //Content of XML file (albums)
 
+	public function __construct($dir = ".")
+	{
+
+		$this->$dir = $dir;
+
+	}
 
 }
 
--- php.php
+++ php.php
@@ -2,4 +2,6 @@
 
 include('php.class.php');
 
+$pm = new picturemess(".");
+
 ?>
